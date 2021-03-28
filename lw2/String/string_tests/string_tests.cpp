667af3745a44c1e5198b692fcf8ff77cd3773884
Alexei Sadovin <sad_alexxxei@mail.ru>
2021-03-29 01:55:31 +0300

    Update string_tests.cpp

--- lw2/String/string_tests/string_tests.cpp
+++ lw2/String/string_tests/string_tests.cpp
@@ -6,20 +6,20 @@
 
 SCENARIO("String Replace")
 {
-	REQUIRE(StringReplacer("", "123", ",") == "");
-	REQUIRE(StringReplacer("12345", "", ",") == "12345");
-	REQUIRE(StringReplacer("1 2 3 4 5", " ", ",") == "1,2,3,4,5");
+	CHECK(StringReplacer("", "123", ",") == "");
+	CHECK(StringReplacer("12345", "", ",") == "12345");
+	CHECK(StringReplacer("1 2 3 4 5", " ", ",") == "1,2,3,4,5");
 }
 
 SCENARIO("HTML Decode")
 {
-	REQUIRE(HtmlDecode("123") == "123");
+	CHECK(HtmlDecode("123") == "123");
 
-	REQUIRE(HtmlDecode("&quot;") == "\"");
-	REQUIRE(HtmlDecode("&apos;") == "’");
-	REQUIRE(HtmlDecode("&lt;") == "<");
-	REQUIRE(HtmlDecode("&gt;") == ">");
-	REQUIRE(HtmlDecode("&amp;") == "&");
+	CHECK(HtmlDecode("&quot;") == "\"");
+	CHECK(HtmlDecode("&apos;") == "’");
+	CHECK(HtmlDecode("&lt;") == "<");
+	CHECK(HtmlDecode("&gt;") == ">");
+	CHECK(HtmlDecode("&amp;") == "&");
 
-	REQUIRE(HtmlDecode("Cat &lt;says&gt; &quot;Meow&quot;. M&amp;M&apos;s") == "Cat <says> \"Meow\". M&M’s");
+	CHECK(HtmlDecode("Cat &lt;says&gt; &quot;Meow&quot;. M&amp;M&apos;s") == "Cat <says> \"Meow\". M&M’s");
 }
